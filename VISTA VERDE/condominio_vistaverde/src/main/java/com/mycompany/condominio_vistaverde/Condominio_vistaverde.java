d581e0bf90ff6fa36f4d7f54522b5f5a75c2ad2a
Leo123-gif <josueperec1@gmail.com>
2026-05-28 19:42:52 -0600

    Add files via upload

---
+++ VISTA VERDE/condominio_vistaverde/src/main/java/com/mycompany/condominio_vistaverde/Condominio_vistaverde.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.condominio_vistaverde;
+
+/**
+ *
+ * @author Josue
+ */
+public class Condominio_vistaverde {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
